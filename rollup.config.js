18aecbe7befde1cd9e87be6e613a452d81633169
Peter Siska <psiska@inventage.com>
2020-04-15 13:54:47 +0200

    Add compatibility config to support down to IE11 (when testing building).

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,3 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
+import { createCompatibilityConfig } from '@open-wc/building-rollup';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
-
-export default createDefaultConfig({ input: './index.html' });
+export default createCompatibilityConfig({ input: './index.html' });
